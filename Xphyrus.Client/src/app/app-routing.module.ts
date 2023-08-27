efeee19eddabee26a35cc8dc13f2711695f915c6
arya2004 <arya20j4@gmail.com>
2023-08-28 01:10:18 +0530

    client: added acc

--- Xphyrus.Client/src/app/app-routing.module.ts
+++ Xphyrus.Client/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
 
 const routes: Routes = [
-  {path: '', component:HomeComponent}
+  {path: '', component:HomeComponent},
+  {path: 'account/login', component:LoginComponent},
+  {path: 'account/register', component: RegisterComponent}
 ];
 
 @NgModule({
