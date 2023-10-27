a5131bb2049a72d6715b78893f693b28bd29cf4b
arya2004 <arya20j4@gmail.com>
2023-10-27 21:21:49 +0530

    fix: working 3 instances of Redis

---
+++ Xphyrus.SubmissionAPI/Models/Dtos/AnswerSubmissionFinalDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Xphyrus.SubmissionAPI.Models.Dtos
+{
+    public class AnswerSubmissionFinalDto
+    {
+        public string? AssignmentId { get; set; }
+        IEnumerable<CodingSubmissionsDto>? CodingSubmissions { get; set; }
+        IEnumerable<MCQSubmissionDto>? MCQSubmissions { get; set;}
+
+    }
+
+    public class CodingSubmissionsDto
+    {
+        public string? source_code { get; set; }
+        public int? language_id { get; set; }
+      
+        public string? CodingId { get; set; }
+    }
+     public class MCQSubmissionDto
+    {
+        public string? MCQId { get; set; }
+        public int MarkedAnswer { get; set; }
+    }
+
+}
