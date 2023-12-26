58d87755ba6cd4972763881bc97b22e3d087c842
arya2004 <arya20j4@gmail.com>
2023-12-27 02:03:59 +0530

    py, go, shell exec

--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/Xphurus/rce-engine/handler"
 	"github.com/runabol/tork/cli"
 	"github.com/runabol/tork/conf"
+	"github.com/runabol/tork/engine"
 )
 
 func main() {
-   // Load the Tork config file (if exists) 
-   if err := conf.LoadConfig(); err != nil {
-     fmt.Println(err)
-     os.Exit(1)
-   }
+	if err := conf.LoadConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-   // Start the Tork CLI
-   app := cli.New()
-   if err := app.Run(); err != nil {
-     fmt.Println(err)
-     os.Exit(1)
-   }
+	engine.RegisterEndpoint(http.MethodPost, "/execute", handler.Handler)
+
+	if err := cli.New().Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
+
+//go run main.go run standalone
