e0dbee698410a37d9285cca7ea4b3aa97e48d993
arya2004 <arya20j4@gmail.com>
2024-07-22 02:30:05 +0530

    refactor: Schema

--- NexusAPI/Models/ApplicationUser.cs
+++ NexusAPI/Models/ApplicationUser.cs
@@ -5,38 +5,24 @@ namespace NexusAPI.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string? DisplayName { get; set; }
-        public string? CompanyName { get; set; }
-        public string? WorkEmail { get; set; }
-        
-        public string? Location { get; set; }
-        public string? Market { get; set; }
-        public string? OneLinePitch { get; set; }
-        public CompanySize CompanySize { get; set; }
-
-        // socials
-        public string? WebsiteUrl { get; set; }
-        public string? TwitterUrl { get; set; }
-        public string? FacebookUrl { get; set; }
-        public string?  LinkedinUrl { get; set; }
-
-        //user
-        public string? Role { get; set; }
-
-        public List<Nexus>? Nexus { get; }
-      
+        public string DisplayName { get; set; }
+        public UserRole Role { get; set; }
+        public ICollection<Classroom> Classrooms { get; set; } = new List<Classroom>();
+        public string ProfilePictureUrl { get; set; }
+        public string Bio { get; set; }
+       
     }
 
-    public enum CompanySize
+    public enum UserRole
     {
-        Small_1_10 = 1,
-        Small_11_50 = 2,
-        Medium_51_200 = 3,
-        Medium_201_500 = 4,
-        Large_501_1000 = 5,
-        Large_1001_5000 = 6,
-        Enterprise_5000Plus = 7
+        Student,
+        Faculty,
+        Admin
     }
+
+
+
+
 
 
 
---
+++ NexusAPI/Models/CodingQuestion.cs
@@ -0,0 +1,15 @@
+﻿namespace NexusAPI.Models
+{
+    public class CodingQuestion
+    {
+        public Guid CodingQuestionId { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid CodingAssessmentId { get; set; }
+        public CodingAssessment CodingAssessment { get; set; }
+        public string Difficulty { get; set; }
+     
+        public ICollection<TestCase> TestCases { get; set; } = new List<TestCase>();
+    }
+
+}
